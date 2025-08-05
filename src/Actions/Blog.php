5de2e89d213ea455d8c371d69684c8640f4cf17c
Aimeos <aimeos@aimeos.org>
2025-08-05 08:57:37 +0200

    Fixed showing blog articles in for editors

--- src/Actions/Blog.php
+++ src/Actions/Blog.php
@@ -17,7 +17,16 @@ class Blog
         $order = $sort[0] === '-' ? substr( $sort, 1 ) : $sort;
         $dir = $sort[0] === '-' ? 'desc' : 'asc';
 
-        $builder = Page::where( 'parent_id', $pid )->where( 'status', 1 )->orderBy( $order, $dir );
+        $builder = Page::where( 'parent_id', $pid )->orderBy( $order, $dir );
+
+        if( \Aimeos\Cms\Permission::can( 'page:view', $request->user() ) ) {
+            $builder->whereHas('latest', function( $builder ) {
+                $builder->where( 'data->status', 1 );
+            } );
+        } else {
+            $builder->where( 'status', 1 );
+        }
+
         $attr = ['id', 'lang', 'path', 'name', 'title', 'to', 'domain', 'content'];
 
         return $builder->paginate( @$item->data?->limit ?? 10, $attr, 'p' )
