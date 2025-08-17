0be996236223640e430841791e393ad869ddaf95
Aimeos <aimeos@aimeos.org>
2025-08-17 08:02:35 +0200

    Fixed missing footer CSS/JS files

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -110,18 +110,8 @@
         @endif
 
         @yield('main')
+        @yield('footer')
 
-        <footer class="cms-content" data-section="footer">
-            @foreach($content['footer'] ?? [] as $item)
-                @if($el = cmsref($page, $item))
-                    <div id="{{ cmsattr(@$item->id) }}" class="{{ cmsattr(@$el->type) }}">
-                        <div class="container">
-                            @includeFirst(cmsviews($page, $el), cmsdata($page, $el))
-                        </div>
-                    </div>
-                @endif
-            @endforeach
-        </footer>
         <footer class="copyright">
             &copy; {{ date('Y') }} {{ config('app.name') }}
         </footer>
