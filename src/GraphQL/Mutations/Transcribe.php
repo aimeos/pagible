9010b7c0ec9785cdf43674959394ab8e9892667b
Aimeos <aimeos@aimeos.org>
2025-08-28 15:30:07 +0200

    Support audio models that can't create WEBVTT transcriptions

--- src/GraphQL/Mutations/Transcribe.php
+++ src/GraphQL/Mutations/Transcribe.php
@@ -17,6 +17,14 @@ final class Transcribe
      */
     public function __invoke( $rootValue, array $args ): string
     {
+        if( !( ( $upload = $args['file'] ?? null ) && $upload instanceof UploadedFile && $upload->isValid() ) ) {
+            throw new Exception( 'No file uploaded' );
+        }
+
+        if( !str_starts_with( $upload->getMimeType(), 'audio/' ) ) {
+            throw new Exception( 'Only audio files' );
+        }
+
         $provider = config( 'cms.ai.audio' ) ?: 'openai';
         $model = config( 'cms.ai.audio-model' ) ?: 'whisper-1';
 
@@ -26,21 +34,39 @@ final class Transcribe
                 'connect_timeout' => 10,
             ] );
 
-        if( !( ( $upload = $args['file'] ?? null ) && $upload instanceof UploadedFile && $upload->isValid() ) ) {
-            throw new Exception( 'No file uploaded' );
-        }
-
-        if( !str_starts_with( $upload->getMimeType(), 'audio/' ) ) {
-            throw new Exception( 'Only audio files' );
-        }
-
         $file = Audio::fromBase64( base64_encode( $upload->getContent() ), $upload->getMimeType() );
 
         $response = $prism->withInput( $file )
-            ->withProviderOptions([
-                'response_format' => 'vtt',
-            ])->asText();
+            ->withProviderOptions( [
+            'response_format' => 'verbose_json',
+        ] )->asText();
 
-        return $response->text;
+        return $this->webvtt( $response->additionalContent['segments'] ?? [] );
+    }
+
+
+    protected function time( float $seconds ) : string
+    {
+        $hours = floor( $seconds / 3600 );
+        $minutes = floor( ( $seconds % 3600 ) / 60 );
+        $secs = floor( $seconds % 60 );
+        $millis = ( $seconds - floor( $seconds ) ) * 1000;
+
+        return sprintf( "%02d:%02d:%02d.%03d", $hours, $minutes, $secs, $millis );
+    }
+
+
+    protected function webvtt( array $segments ) : string
+    {
+        $lines = ['WEBVTT', ''];
+
+        foreach( $segments as $segment )
+        {
+            $lines[] = $this->time( $segment['start'] ) . ' --> ' . $this->time( $segment['end'] );
+            $lines[] = trim( $segment['text'] );
+            $lines[] = "";
+        }
+
+        return implode( "\n", $lines );
     }
 }
