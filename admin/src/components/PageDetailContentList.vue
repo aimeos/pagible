0892d84a2856003e8fb3ed72f38979fc3dbecc14
Aimeos <aimeos@aimeos.org>
2025-08-31 13:58:52 +0200

    Added help for page content AI input field

--- admin/src/components/PageDetailContentList.vue
+++ admin/src/components/PageDetailContentList.vue
@@ -30,6 +30,7 @@
     data: () => ({
       chat: '',
       response: '',
+      help: false,
       shortmsg: true,
       refining: false,
       panel: [],
@@ -590,6 +591,13 @@
       outlined
       rows="1"
     >
+      <template #prepend>
+        <v-icon @click="help = !help">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+          </svg>
+        </v-icon>
+      </template>
       <template #append>
         <v-icon @click="refining || refine()"
           @keydown.enter="refining || refine()"
@@ -608,6 +616,12 @@
         </v-icon>
       </template>
     </v-textarea>
+    <div v-if="help" class="help">
+      <ul>
+        <li>{{ $gettext('AI can add or improve content based on your input') }}</li>
+        <li>{{ $gettext('It can take a long time depending on the task and content size') }}</li>
+      </ul>
+    </div>
 
     <div class="header">
       <div v-if="auth.can('page:save')" class="bulk">
@@ -822,5 +836,13 @@
 .icon-shared {
   color: rgb(var(--v-theme-warning));
   margin-inline-end: 4px;
+}
+
+.help {
+  color: rgb(var(--v-theme-on-surface));
+  background-color: rgb(var(--v-theme-surface-light));
+  padding: 16px 24px 16px 32px ;
+  margin-bottom: 16px;
+  border-radius: 8px;
 }
 </style>
