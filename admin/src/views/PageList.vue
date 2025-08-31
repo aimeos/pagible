dd35f0c8a453c2872dbfcd596c0fe3438075b1a3
Aimeos <aimeos@aimeos.org>
2025-08-31 09:19:39 +0200

    Make synthesize/refine fields usable by keyboard and added hint

--- admin/src/views/PageList.vue
+++ admin/src/views/PageList.vue
@@ -200,7 +200,9 @@
             </v-icon>
           </template>
           <template #append>
-            <v-icon @click="synthesizing || synthesize()">
+            <v-icon @click="synthesizing || synthesize()"
+              @keydown.enter="synthesizing || synthesize()"
+              :title="synthesizing ? $gettext('Generating ...') : $gettext('Generate page based on prompt')">
               <svg v-if="synthesizing === false" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M6,13H14L10.5,16.5L11.92,17.92L17.84,12L11.92,6.08L10.5,7.5L14,11H6V13Z" />
               </svg>
