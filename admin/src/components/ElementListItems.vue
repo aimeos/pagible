cb43dc123de444ab4107344cfe24e73e2f8866de
Aimeos <aimeos@aimeos.org>
2025-09-01 10:33:21 +0200

    Improved layout for schema item dialogs

--- admin/src/components/ElementListItems.vue
+++ admin/src/components/ElementListItems.vue
@@ -562,7 +562,11 @@
 
   <Teleport to="body">
     <v-dialog v-model="vschemas" @afterLeave="vschemas = false" scrollable width="auto">
-      <SchemaItems type="content" @add="add($event)" />
+      <v-card>
+        <v-card-text>
+          <SchemaItems type="content" @add="add($event)" />
+        </v-card-text>
+      </v-card>
     </v-dialog>
   </Teleport>
 </template>
