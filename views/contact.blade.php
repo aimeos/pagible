715ff427e76c5377bf0801169c2469d9a2eb0f44
Aimeos <aimeos@aimeos.org>
2025-09-23 08:45:53 +0200

    Improved contact form

--- views/contact.blade.php
+++ views/contact.blade.php
@@ -21,21 +21,30 @@
             <input id="email" type="email" name="email" placeholder="{{ __('Your e-mail address') }}" required />
         </div>
     </div>
-    <div class="col">
-        <label for="message">{{ __('Message') }}</label>
-        <textarea id="message" name="message" placeholder="{{ __('Your message') }}" required rows="6"></textarea>
+    <div class="row">
+        <div class="col">
+            <label for="message">{{ __('Message') }}</label>
+            <textarea id="message" name="message" placeholder="{{ __('Your message') }}" required rows="6"></textarea>
+        </div>
     </div>
-
-    @if(!app()->environment('local') && config('services.hcaptcha.sitekey'))
-        <div class="h-captcha" data-sitekey="{{ config('services.hcaptcha.sitekey') }}" data-theme="dark"></div>
-    @endif
-
-    <div class="errors"></div>
-
-    <button type="submit" class="btn">
-        <span class="send">{{ __('Send message') }}</span>
-        <span class="sending hidden"aria-busy="true">{{ __('Message will be sent') }}</span>
-        <span class="success hidden">{{ __('Successfully sent') }}</span>
-        <span class="failure hidden">{{ __('Error sending e-mail') }}</span>
-    </button>
+    <div class="row">
+        <div class="col">
+            <div class="errors"></div>
+        </div>
+    </div>
+    <div class="row">
+        @if(!app()->environment('local') && config('services.hcaptcha.sitekey'))
+            <div class="col">
+                <div class="h-captcha" data-sitekey="{{ config('services.hcaptcha.sitekey') }}"></div>
+            </div>
+        @endif
+        <div class="col">
+            <button type="submit" class="btn">
+                <span class="send">{{ __('Send message') }}</span>
+                <span class="sending hidden"aria-busy="true">{{ __('Message will be sent') }}</span>
+                <span class="success hidden">{{ __('Successfully sent') }}</span>
+                <span class="failure hidden">{{ __('Error sending e-mail') }}</span>
+            </button>
+        </div>
+    </div>
 </form>
