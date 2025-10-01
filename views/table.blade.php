853608b6ae3d18e9b66d51e75bc39c64eb0a10e4
Aimeos <aimeos@aimeos.org>
2025-10-01 14:29:48 +0200

    Improved table layout and avoid Bootstrap conflicts

--- views/table.blade.php
+++ views/table.blade.php
@@ -1,11 +1,11 @@
 @if(@$data->title)
 	<h2>{{ $data->title }}</h2>
 @endif
-<div class="table">
+<div class="table-responsive">
 	@foreach(@$data->table ?? [] as $rowidx => $row)
-		<div class="row">
+		<div class="table-row">
 			@foreach((array) $row as $colidx => $col)
-				<div class="col {{
+				<div class="table-col {{
 					$colidx === 0 && in_array(@$data->header, ['col', 'row+col']) ||
 					$rowidx === 0 && in_array(@$data->header, ['row', 'row+col']) ? 'th' : 'td'
 				}}">
