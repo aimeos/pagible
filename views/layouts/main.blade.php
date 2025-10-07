29fcfaef10fafd09a30be0ad0e9a5b5394e05c4c
Aimeos <aimeos@aimeos.org>
2025-10-07 13:57:02 +0200

    Implemented page search in frontend

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -30,6 +30,7 @@
 
         <link href="{{ cmsasset('vendor/cms/theme/pico.min.css') }}" rel="stylesheet">
         <link href="{{ cmsasset('vendor/cms/theme/pico.nav.min.css') }}" rel="stylesheet">
+        <link href="{{ cmsasset('vendor/cms/theme/pico.modal.min.css') }}" rel="stylesheet">
         <link href="{{ cmsasset('vendor/cms/theme/pico.dropdown.min.css') }}" rel="stylesheet">
         <link href="{{ cmsasset('vendor/cms/theme/cms.css') }}" rel="stylesheet">
         @stack('css')
@@ -43,19 +44,33 @@
         @endforeach
     </head>
     <body class="theme-{{ cms($page, 'theme') ?: 'cms' }} type-{{ cms($page, 'type') ?: 'page' }}">
+        <dialog id="modal-search" class="search">
+            <article>
+                <form action="{{ route('cms.search', ['search' => '_term_']) }}">
+                    <input id="modal-search-input" placeholder="{{ __('Search website') }}">
+                    <button type="reset" aria-label="{{ __('Close') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                        </svg>
+                    </button>
+                </form>
+                <div class="results">
+                </div>
+            </article>
+        </dialog>
         <header>
             <nav>
                 <ul>
                     <li class="sidebar-open show">
                         <button aria-label="{{ __('Open sidebar') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
                             </svg>
                         </button>
                     </li>
                     <li class="sidebar-close">
                         <button aria-label="{{ __('Close sidebar') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
                             </svg>
                         </button>
@@ -74,13 +89,20 @@
                     </li>
                     <li class="menu-close">
                         <button aria-label="{{ __('Close navigation') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                             </svg>
                         </button>
                     </li>
                 </ul>
                 <ul class="menu">
+                    <li>
+                        <a href="#" class="search contrast" data-modal="modal-search">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                            </svg>
+                        </a>
+                    </li>
                     @foreach($page->nav() as $item)
                         @if(cms($item, 'status') == 1)
                             <li>
@@ -111,7 +133,7 @@
                 <ul class="menu-open show">
                     <li>
                         <button aria-label="{{ __('Open navigation') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                             </svg>
                         </button>
