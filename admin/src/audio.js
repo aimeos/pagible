4d61aba8d55a7d6c58e585fc1d094dafadf49dd5
Aimeos <aimeos@aimeos.org>
2025-10-15 16:11:16 +0200

    Fixed audio processing with active CSP

--- admin/src/audio.js
+++ admin/src/audio.js
@@ -18,7 +18,7 @@ export function recording() {
 
       audioContext = new AudioContext()
 
-      await audioContext.audioWorklet.addModule(new URL('./recorder.js', import.meta.url))
+      await audioContext.audioWorklet.addModule('/recorder.js')
 
       node = new AudioWorkletNode(audioContext, 'recorder-processor')
 
