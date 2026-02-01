195d32133f8f8f0b62df769c046aeb6ca1cd4a6e
Aimeos <aimeos@aimeos.org>
2026-02-01 16:39:27 +0100

    Moved transactions from models to commands

--- src/Models/Element.php
+++ src/Models/Element.php
@@ -19,7 +19,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
 
 
@@ -171,19 +170,15 @@ class Element extends Model
      */
     public function publish( Version $version ) : self
     {
-        DB::connection( $this->getConnectionName() )->transaction( function() use ( $version ) {
+        $this->files()->sync( $version->files ?? [] );
 
-            $this->files()->sync( $version->files ?? [] );
+        $this->fill( (array) $version->data );
+        $this->editor = $version->editor;
+        $this->lang = $version->lang;
+        $this->save();
 
-            $this->fill( (array) $version->data );
-            $this->editor = $version->editor;
-            $this->lang = $version->lang;
-            $this->save();
-
-            $version->published = true;
-            $version->save();
-
-        }, 3 );
+        $version->published = true;
+        $version->save();
 
         return $this;
     }
