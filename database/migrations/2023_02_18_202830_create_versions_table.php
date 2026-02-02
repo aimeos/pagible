948ef8b9d06531a302ae5fdcc51940654a8ce3fd
Aimeos <aimeos@aimeos.org>
2026-02-02 18:00:29 +0100

    Changed primary definition for ID columns

--- database/migrations/2023_02_18_202830_create_versions_table.php
+++ database/migrations/2023_02_18_202830_create_versions_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
     public function up()
     {
         Schema::connection(config('cms.db', 'sqlite'))->create('cms_versions', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('tenant_id');
             $table->uuid('versionable_id');
             $table->string('versionable_type', 50);
@@ -31,7 +31,6 @@ return new class extends Migration
             $table->string('editor');
             $table->timestamp('created_at');
 
-            $table->primary('id');
             $table->index(['versionable_id', 'versionable_type', 'created_at', 'tenant_id'], 'idx_versions_id_type_created_tenantid');
             $table->index(['publish_at', 'published']);
         });
