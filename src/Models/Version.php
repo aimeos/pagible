00047ca66b4297d4265f0fc05858bd561126d67a
Aimeos <aimeos@aimeos.org>
2026-02-09 14:53:36 +0100

    Use milliseconds for created_at in Version model

--- src/Models/Version.php
+++ src/Models/Version.php
@@ -51,10 +51,12 @@ class Version extends Model
     protected $casts = [
         'data' => 'object',
         'aux' => 'object',
-        'created_at' => 'datetime:Y-m-d H:i:s.u',
     ];
 
-    protected $dateFormat = 'Y-m-d H:i:s.u';
+    /**
+     * The date format with milliseconds used by created_at.
+     */
+    protected $dateFormat = 'Y-m-d H:i:s.v';
 
     /**
      * The attributes that are mass assignable.
