948581d3bac791fd1b5e3242169028402deb76da
Aimeos <aimeos@aimeos.org>
2026-03-01 13:09:49 +0100

    Implemented setting user permissions on CLI

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -12,6 +12,7 @@ use Aimeos\Cms\Models\File;
 use Aimeos\Cms\Models\Element;
 use Aimeos\Cms\Models\Content;
 use Database\Seeders\CmsSeeder;
+use Illuminate\Foundation\Auth\User;
 
 
 class CommandTest extends TestAbstract
@@ -38,5 +39,40 @@ class CommandTest extends TestAbstract
             'en' => 'Test file description',
             'de' => 'Beschreibung der Testdatei',
         ], File::where( 'mime', 'image/jpeg' )->firstOrFail()?->description );
+    }
+
+
+    public function testUser(): void
+    {
+        $this->seed( CmsSeeder::class );
+
+        $this->artisan('cms:user', ['-p' => 'test', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-e' => true, 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0b00000000_01111111_00000001_11000111_00000001_11111111_01111111_11111111, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-d' => true, 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-a' => '*', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0b00000000_01111111_00000001_11000111_00000001_11111111_01111111_11111111, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-r' => '*', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-a' => 'image:*', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0b00000000_01111111_00000000_00000000_00000000_00000000_00000000_00000000, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-r' => 'image:*', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-a' => '*:view', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0b00000000_00000000_00000000_00000000_00000000_00000001_00000001_00000001, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-r' => '*:view', 'email' => 'test@example.com'])->assertExitCode( 0 );
+        $this->assertEquals( 0, User::where('email', 'test@example.com')->get()->first()?->cmseditor );
+
+        $this->artisan('cms:user', ['-l' => true, 'email' => 'test@example.com'])->assertExitCode( 0 );
     }
 }
