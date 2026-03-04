45b093352b35cfdafbad6d401eb106abaabd5321
Aimeos <aimeos@aimeos.org>
2026-03-04 10:46:39 +0100

    Added Cyress tests for admin backend (#56)

--- admin/cypress.config.js
+++ admin/cypress.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  allowCypressEnv: false,
   e2e: {
     baseUrl: 'http://localhost:4173',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
