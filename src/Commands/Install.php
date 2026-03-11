7b4922a8206b29e5ef668e735d6386cae3d9e8fd
Aimeos <aimeos@aimeos.org>
2026-03-11 20:58:33 +0100

    Use "cms" scout engine on installation

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -53,6 +53,9 @@ Made with <fg=green>love</> by the Pagible CMS community. Be a part of it!
         $this->comment( '  Publishing Laravel Scout files ...' );
         $result += $this->call( 'vendor:publish', ['--provider' => 'Laravel\Scout\ScoutServiceProvider'] );
 
+        $this->comment( '  Updating Scout configuration ...' );
+        $result += $this->scout();
+
         $this->comment( '  Publishing JSON:API configuration ...' );
         $result += $this->call( 'vendor:publish', ['--provider' => 'LaravelJsonApi\Laravel\ServiceProvider'] );
 
@@ -286,6 +289,39 @@ Made with <fg=green>love</> by the Pagible CMS community. Be a part of it!
         if( $done ) {
             file_put_contents( base_path( $filename ), $content );
         } else {
+            $this->line( sprintf( '  File [%1$s] already up to date' . PHP_EOL, $filename ) );
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Updates Scout configuration
+     *
+     * @return int 0 on success, 1 on failure
+     */
+    protected function scout() : int
+    {
+        $filename = 'config/scout.php';
+        $content = file_get_contents( base_path( $filename ) );
+
+        if( $content === false ) {
+            $this->error( "  File [$filename] not found!" );
+            return 1;
+        }
+
+        $search = "env('SCOUT_DRIVER', 'collection')";
+        $replace = "env('SCOUT_DRIVER', 'cms')";
+
+        if( strpos( $content, $replace ) === false )
+        {
+            $content = str_replace( $search, $replace, $content );
+            file_put_contents( base_path( $filename ), $content );
+            $this->line( sprintf( '  Updated default Scout driver to "cms" in [%1$s]' . PHP_EOL, $filename ) );
+        }
+        else
+        {
             $this->line( sprintf( '  File [%1$s] already up to date' . PHP_EOL, $filename ) );
         }
 
