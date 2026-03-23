f71b0363e4c06d512ff5f4feae998f6bea9508db
Aimeos <aimeos@aimeos.org>
2026-03-23 12:06:34 +0100

    PHPStan

--- src/Commands/User.php
+++ src/Commands/User.php
@@ -67,7 +67,7 @@ class User extends Command
         }
 
         if( $perms = $this->option( 'add' ) ) {
-            $user = Permission::add( $this->permissions( $perms ), $user );
+            $user = Permission::add( $this->permissions( $perms ), $user ); // @phpstan-ignore-line argument.type
         }
 
         if( is_string( $role = $this->option( 'role' ) ) ) {
@@ -75,7 +75,7 @@ class User extends Command
         }
 
         if( $perms = $this->option( 'remove' ) ) {
-            $user = Permission::remove( $this->permissions( $perms ), $user );
+            $user = Permission::remove( $this->permissions( $perms ), $user ); // @phpstan-ignore-line argument.type
         }
 
         if( $this->option( 'disable' ) ) {
