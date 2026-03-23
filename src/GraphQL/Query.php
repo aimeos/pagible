34205ff783453753d2937f45b4da2ebe7688ce8c
Aimeos <aimeos@aimeos.org>
2026-03-23 16:30:24 +0100

    PHPStan update

--- src/GraphQL/Query.php
+++ src/GraphQL/Query.php
@@ -201,8 +201,13 @@ final class Query
             $builder->where( 'cms_versions.editor', (string) $filter['editor'] );
         }
 
-        if( isset( $filter['any'] ) ) {
-            $ids = $model::search( mb_substr( trim( $filter['any'] ), 0, 200 ) )->searchFields( 'draft' )->take( 1000 )->keys(); // @phpstan-ignore method.notFound
+        if( isset( $filter['any'] ) )
+        {
+            $ids = $model::search( mb_substr( trim( $filter['any'] ), 0, 200 ) )
+                ->searchFields( 'draft' ) // @phpstan-ignore method.notFound
+                ->take( 1000 )
+                ->keys();
+
             $builder->whereIn( $table . '.id', $ids->all() );
         }
     }
