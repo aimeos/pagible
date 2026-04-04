bb110c2d2db99cfed08a407db1e6a4e441e34488
Aimeos <aimeos@aimeos.org>
2026-04-04 16:43:29 +0200

    Debug output for benchmarks

--- tests/BenchmarkTest.php
+++ tests/BenchmarkTest.php
@@ -68,7 +68,7 @@ class BenchmarkTest extends CmsTestAbstract
             '--tries' => 10,
             '--chunk' => $chunk,
             '--force' => true,
-            '-v' => true,
+            '-vv' => true,
         ], $output );
 
         $this->assertSame( 0, $run, 'Benchmark run failed: ' . Artisan::output() );
