10f88ab013dc5e6cb21d99e22f89bf17cdf66fcf
Aimeos <aimeos@aimeos.org>
2026-04-05 17:56:18 +0200

    MariaDB issue

--- core/database/migrations/2026_04_05_500000_add_version_id_filter_indexes.php
+++ core/database/migrations/2026_04_05_500000_add_version_id_filter_indexes.php
@@ -39,7 +39,7 @@ return new class extends Migration
 
         if (in_array($driver, ['mysql', 'mariadb'])) {
             $db->statement("ALTER TABLE cms_versions
-                ADD COLUMN data_scheduled BOOLEAN GENERATED ALWAYS AS (JSON_VALUE(data, '$.scheduled') = 'true') VIRTUAL,
+                ADD COLUMN data_scheduled BOOLEAN GENERATED ALWAYS AS (IF(JSON_EXTRACT(data, '$.scheduled'), 1, 0)) VIRTUAL,
                 ADD COLUMN data_name VARCHAR(255) GENERATED ALWAYS AS (JSON_VALUE(data, '$.name')) VIRTUAL
             ");
 
