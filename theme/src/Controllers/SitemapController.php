cfb9da4b6be6d6f4ec195a0f88e1e77afdd9994e
Aimeos <aimeos@aimeos.org>
2026-04-07 11:26:52 +0200

    Further improvements

--- theme/src/Controllers/SitemapController.php
+++ theme/src/Controllers/SitemapController.php
@@ -10,7 +10,6 @@ namespace Aimeos\Cms\Controllers;
 use Aimeos\Cms\Models\Nav;
 use Aimeos\Cms\Scopes\Status;
 use Illuminate\Routing\Controller;
-use Illuminate\Support\Carbon;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 
@@ -18,31 +17,37 @@ class SitemapController extends Controller
 {
     public function index() : StreamedResponse
     {
-        return response()->stream( function() {
+        $tz = new \DateTimeZone( config('app.timezone') ?: 'UTC' );
+        $multidomain = config( 'cms.multidomain' ) ? ['domain' => '__CMS_DOMAIN__'] : [];
+        $template = route( 'cms.page', $multidomain + ['path' => '__CMS_PATH__'] );
+
+        return response()->stream( function() use ($tz, $template) {
             echo '<?xml version="1.0" encoding="UTF-8"?>';
             echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-            $multidomain = (bool) config('cms.multidomain');
             $query = Nav::withGlobalScope('status', new Status)
-                ->select( 'path', 'domain', 'to', 'updated_at' )
+                ->select( 'path', 'domain', 'updated_at' )
+                ->where( function( $q ) {
+                    $q->whereNull( 'to' )->orWhere( 'to', '' );
+                } )
                 ->toBase();
 
             $i = 0;
             foreach( $query->cursor() as $page )
             {
-                if( $page->to ) {
-                    continue;
-                }
+                $lastmod = $page->updated_at
+                    ? date_create_immutable( $page->updated_at, $tz )->format( \DateTimeInterface::ATOM )
+                    : '';
 
-                $lastmod = $page->updated_at ? Carbon::parse($page->updated_at)->toAtomString() : '';
-                $loc = route('cms.page', ['path' => $page->path] + ($multidomain ? ['domain' => $page->domain] : []));
+                $encodedPath = implode( '/', array_map( 'rawurlencode', explode( '/', (string) $page->path ) ) );
+                $loc = str_replace( ['__CMS_PATH__', '__CMS_DOMAIN__'], [$encodedPath, $page->domain ?? ''], $template );
 
                 echo '<url>';
-                echo '<loc><![CDATA[' . str_replace(']]>', ']]]]><![CDATA[>', $loc) . ']]></loc>';
+                echo '<loc><![CDATA[' . $loc . ']]></loc>';
                 echo '<lastmod><![CDATA[' . $lastmod . ']]></lastmod>';
                 echo '</url>';
 
-                if( ++$i % 100 === 0 ) {
+                if( ++$i % 5000 === 0 ) {
                     flush();
                 }
             }
