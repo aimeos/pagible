a0a97ea2f67e0f766362cdf7f4c3f789c825a21e
Aimeos <aimeos@aimeos.org>
2026-04-09 11:51:00 +0200

    Fixed statement

--- core/src/Concerns/Benchmarks.php
+++ core/src/Concerns/Benchmarks.php
@@ -168,7 +168,7 @@ trait Benchmarks
             if( $driver === 'sqlsrv' )
             {
                 $fullSql = array_reduce($bindings, function (string $carry, mixed $binding): string {
-                    $value = is_numeric($binding) ? $binding : "'" . addslashes((string) $binding) . "'";
+                    $value = is_numeric( $bindings[$i] ) ? $bindings[$i] : "'" . str_replace( "'", "''", (string) $bindings[$i] ) . "'";
                     return preg_replace('/\?/', $value, $carry, 1);
                 }, $sql);
 
