a289c4c43eb90f6a4f3852ba756d1102f3a9ac0b
Aimeos <aimeos@aimeos.org>
2026-04-10 17:38:42 +0200

    Use collection engine for theme search tests

--- search/tests/CmsEngineTest.php
+++ search/tests/CmsEngineTest.php
@@ -34,10 +34,20 @@ class CmsEngineTest extends SearchTestAbstract
     protected function setUp(): void
     {
         parent::setUp();
-        $this->seed( CmsSeeder::class );
 
-        if( DB::connection( config( 'cms.db' ) )->getDriverName() === 'sqlsrv' ) {
-            sleep( 5 );
+        $this->seed( CmsSeeder::class );
+        $conn = DB::connection( config( 'cms.db' ) );
+
+        if( $conn->getDriverName() === 'sqlsrv' )
+        {
+            $conn->statement( 'ALTER FULLTEXT INDEX ON cms_index START FULL POPULATION' );
+
+            for( $i = 0; $i < 10; $i++ )
+            {
+                if( !$conn->scalar( "SELECT FULLTEXTCATALOGPROPERTY('cms_index_catalog', 'PopulateStatus')" ) ) {
+                    sleep( 1 );
+                }
+            }
         }
     }
 
