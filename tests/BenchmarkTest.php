2647c29506fa2350a408e20435d754242810e12b
Aimeos <aimeos@aimeos.org>
2026-04-15 17:39:31 +0200

    Re-enabled all CI tests

--- tests/BenchmarkTest.php
+++ tests/BenchmarkTest.php
@@ -66,20 +66,12 @@ class BenchmarkTest extends CmsTestAbstract
 
         $this->assertSame( 0, $seed, 'Seeding failed: ' . Artisan::output() );
 
-        $run = Artisan::call( 'cms:benchmark:theme', [
+        $run = Artisan::call( 'cms:benchmark', [
             '--domain' => 'benchmark',
             '--tries' => 10,
             '--chunk' => $chunk,
             '--force' => true,
-            '-vvv' => true,
-        ], $output );
-
-        $run += Artisan::call( 'cms:benchmark:search', [
-            '--domain' => 'benchmark',
-            '--tries' => 10,
-            '--chunk' => $chunk,
-            '--force' => true,
-            '-vvv' => true,
+            '-v' => true,
         ], $output );
 
         $this->assertSame( 0, $run, 'Benchmark run failed: ' . Artisan::output() );
