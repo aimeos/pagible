74205f973445aec9e6b0b16b48641454a097b527
Aimeos <aimeos@aimeos.org>
2026-04-15 10:06:47 +0200

    Hide aria instructions

--- admin/js/components/PageListItems.vue
+++ admin/js/components/PageListItems.vue
@@ -1640,4 +1640,13 @@ export default {
     padding: 4px 0;
   }
 }
+
+.sr-only {
+  position: absolute;
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  white-space: nowrap;
+}
 </style>
