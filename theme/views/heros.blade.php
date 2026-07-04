c1b0cc0b49d749734edad03bed68118650ac23d4
Aimeos <aimeos@aimeos.org>
2026-07-04 11:25:05 +0200

    Added content element for list of pages with hero elements (#249)
    BREANKING: Renames blog CSS/JS files

--- theme/views/heros.blade.php
+++ theme/views/heros.blade.php
@@ -11,38 +11,21 @@
         @if(($data->layout ?? 'default') === 'default')
             <div class="list-items list-default" data-list="{{ $data->{'parent-page'}?->value ?? '' }}">
                 <div class="first">
-                    @include('cms::list-item', ['item' => $first, 'layout' => 'cards'])
+                    @include('cms::list-item', ['item' => $first, 'layout' => 'cards', 'date' => false])
                 </div>
                 <div class="second">
                     @foreach($action?->skip(1) ?? [] as $item)
-                        @include('cms::list-item', ['item' => $item, 'layout' => $data->layout ?? 'default'])
+                        @include('cms::list-item', ['item' => $item, 'layout' => $data->layout ?? 'default', 'date' => false])
                     @endforeach
                 </div>
             </div>
         @else
             <div class="list-items list-{{ $data->layout ?? 'default' }}" data-list="{{ $data->{'parent-page'}?->value ?? '' }}">
                 @foreach($action ?? [] as $item)
-                    @include('cms::list-item', ['item' => $item, 'layout' => $data->layout ?? 'default'])
+                    @include('cms::list-item', ['item' => $item, 'layout' => $data->layout ?? 'default', 'date' => false])
                 @endforeach
             </div>
         @endif
         {{ $action?->appends(request()->query())?->links() }}
-
-        <script type="application/ld+json">{
-            "@@context": "https://schema.org",
-            "@@type": "Blog",
-            "name": {!! cmsjson($data->title ?? cms($page, 'title')) !!},
-            "blogPost": [
-            @foreach($action ?? [] as $item)
-                {
-                    "@@type": "BlogPosting",
-                    "headline": {!! cmsjson(cms($item, 'title')) !!},
-                    "url": {!! cmsjson(route('cms.page', ['path' => $item->path])) !!},
-                    "datePublished": "{{ $item->created_at->toIso8601String() }}"
-                }
-                @if(!$loop->last),@endif
-            @endforeach
-            ]
-        }</script>
     </div>
 @endif
