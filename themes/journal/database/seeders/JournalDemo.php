af4d659c6eb50685e95f70b0e98acab1f17380db
Aimeos <aimeos@aimeos.org>
2026-09-14 18:20:09 +0200

    Added website config element for storing name of the website (#312)

--- themes/journal/database/seeders/JournalDemo.php
+++ themes/journal/database/seeders/JournalDemo.php
@@ -381,6 +381,7 @@ class JournalDemo extends AbstractDemo
         $logoId = $this->logoFile();
 
         $config = [
+            'website' => Validation::entry( 'website', ['title' => 'Kontur'], 'config' ),
             'logo' => [
                 'type' => 'logo',
                 'files' => [$logoId],
